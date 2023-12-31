9063265f3ea723146a7816125c8bec71de503e53
blackEye <69810869+xrwy@users.noreply.github.com>
2024-01-01 00:49:25 +0300

    Update replicate.dart

--- lib/models/replicate.dart
+++ lib/models/replicate.dart
@@ -1,6 +1,6 @@
 class Replicate {
   final String _apiUrl = "https://api.replicate.com/v1/predictions";
-  final String _apiToken = "r8_aycIYE7Gka8GDLY3UxsLvgm1sx1QgCg3Asgvr";
+  final String _apiToken = "REPLICATE_API_KEY";
 
   String get apiUrl => _apiUrl;
   String get apiToken => _apiToken;
