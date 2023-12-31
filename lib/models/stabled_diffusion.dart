bcc7c670cedc3347757e03c76bd61eae647f61c9
blackEye <69810869+xrwy@users.noreply.github.com>
2024-01-01 00:50:35 +0300

    Update stabled_diffusion.dart

--- lib/models/stabled_diffusion.dart
+++ lib/models/stabled_diffusion.dart
@@ -1,6 +1,6 @@
 class StabledDiffusion {
   final String _apiUrl = 'https://stablediffusionapi.com/api/v4/dreambooth';
-  final String _apiKey = "3F1M4MBsTdcPotGLmInkq7AOcRfr0adrUCmlSLIanRljCFzJi39sYa82mixA";
+  final String _apiKey = "StabledDiffusion_API_KEY";
 
   String get apiUrl => _apiUrl;
   String get apiKey => _apiKey;
